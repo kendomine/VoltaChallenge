47384a843fb5696d4ae732eda044527848e906e8
Kenneth <kendomine@yahoo.com>
2018-06-15 12:05:27 -0700

    Made API call and listed stations.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import StationList from './src/components/StationList';
 
+
+// Create a component
+const App = () => (
+	<View style= {{ flex: 1 }}>
+	  <Header headerText={' Volta Stations'} />
+	  <StationList />
+	</View>
+);
+
+
+
+
+// Render it to the device
 AppRegistry.registerComponent('VoltaChallenge', () => App);
